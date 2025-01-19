a84cfbadd3247ad03f29ec6ad544a17e5500fbec
Renat <renat.zayika@gmail.com>
2025-01-19 14:16:55 +0100

    improved

--- Survival Arena (Online)/Assets/Scripts/Business/Data/AssetPath.cs
+++ Survival Arena (Online)/Assets/Scripts/Business/Data/AssetPath.cs
@@ -7,7 +7,7 @@ namespace Business.Data
         public const string SfxAudioSource = "Prefabs/Audio/sfx_audio_source";
         public const string MusicAudioSource = "Prefabs/Audio/music_audio_source";
         public const string Container = "Prefabs/Base/container";
-        public const string Player = "Prefabs/Game/player";
+        public const string Player = "Prefabs/Game/Player/player";
         public const string Map = "Prefabs/Game/map";
         public const string GameView = "Prefabs/UI/game_view";
         public const string GameCamera = "Prefabs/Game/game_camera";
